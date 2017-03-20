2765075aeaf7d633c295d8e242d8558b3cc4f260
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-20 10:41:25 -0400

    added super(); call to empty task constructor.

--- src/org/onyxplatform/api/java/Task.java
+++ src/org/onyxplatform/api/java/Task.java
@@ -4,18 +4,19 @@ import clojure.lang.PersistentHashMap;
 import java.util.Map;
 
 
-public class Task 
+public class Task
 	extends OnyxEntity
 {
 	protected static String coerceKw = "catalog-entry";
 
     	public Task() {
+			super();
     	}
-   	 
+
     	private Task(PersistentHashMap ent) {
 	    	super(ent);
     	}
-	
+
     	protected PersistentHashMap coerce(Map<String, Object> jMap) {
 		return (PersistentHashMap) super.castTypesFn.invoke( coerceKw, jMap);
     	}
