bfd0e73a44f505f51c51abf55d21dad4ec3a6f0f
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 17:41:25 -0400

    added Window comments, minor code corrections

--- src/org/onyxplatform/api/java/Window.java
+++ src/org/onyxplatform/api/java/Window.java
@@ -3,20 +3,43 @@ package org.onyxplatform.api.java;
 import clojure.lang.PersistentHashMap;
 import java.util.Map;
 
-public class Window
-	extends OnyxEntity
+/**
+ * Windows allow you to group and accrue data into possibly overlapping buckets.
+ * Windows are intimately related to the Triggers feature.
+ * Windowing splits up a possibly unbounded data set into finite, possibly
+ * overlapping portions. Windows allow us create aggregations over distinct
+ * portions of a stream, rather than stalling and waiting for the entire data
+ * data set to arrive.
+ * Window objects are derived from OnyxEntity.
+ */
+public class Window extends OnyxEntity
 {
 	protected static String coerceKw = "window-entry";
 
-    	public Window() {
-	    	super();
-    	}
-    	
-    	private Window(Window e) {
-	    	super( e.entry );
-    	}
-		
-    	protected PersistentHashMap coerce(Map<String, Object> jMap) {
-		return (PersistentHashMap) super.castTypesFn.invoke( coerceKw, jMap);
-    	}
+	/**
+	 * Creates a new Window object using OnyxEntity superconstructor.
+	 * @return new Window object
+	 */
+	public Window() {
+	}
+
+	/**
+	 * Creates a new Window object using an existing Window (uses content map).
+	 * Uses OnyxEntity superconstructor.
+	 * @param  Window e             existing window to use for content
+	 * @return        new Window object
+	 */
+	private Window(Window e) {
+    	super(e.entry);
+	}
+
+	/**
+	 * Coerces Window object content map into proper onyx window.
+	 * Returns the onyx representation without altering the existing content map.
+	 * @param  Map<String, Object>       jMap content map to coerce
+	 * @return             onyx representation of content map
+	 */
+	protected PersistentHashMap coerce(Map<String, Object> jMap) {
+		return (PersistentHashMap) castTypesFn.invoke(coerceKw, jMap);
+	}
 }
