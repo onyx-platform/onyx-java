b3293a40d1ac24f340d64ce964032245ba1c1bbb
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 17:46:26 -0400

    added Windows comments, minor code fixes

--- src/org/onyxplatform/api/java/Windows.java
+++ src/org/onyxplatform/api/java/Windows.java
@@ -1,17 +1,35 @@
 package org.onyxplatform.api.java;
 
-public class Windows 
-	extends OnyxVector
+/**
+ * Windows objects are a set of Window objects that can be used for a
+ * given job.
+ * Windows derive from OnyxVector.
+ */
+public class Windows extends OnyxVector
 {
+	/**
+	 * Creates a new Windows object using OnyxVector superconstructor.
+	 * @return new Windows object
+	 */
     public Windows() {
-	    super();
     }
-    
+
+	/**
+	 * Creates a new Windows object using an existing Windows
+	 * object (uses contents from existing Windows).
+	 * Uses OnyxVector superconstructor.
+	 * @param  Windows wes           existing windows object to use for content
+	 * @return         new Windows object
+	 */
     private Windows(Windows wes) {
-	    super( wes.vContents );
+	    super(wes.vContents);
     }
-    
+
+	/**
+	 * Adds an existing Window object to the Windows content vector.
+	 * @param Window w Window to add
+	 */
     public void addWindow(Window w) {
-	    addElement( w );
+	    addElement(w);
     }
 }
