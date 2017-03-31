d3d7d68a5befce8737ff8cbe9819bc5781de23cb
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 13:55:31 -0400

    Updated API methods and set API.java to implement OnyxNames interface.

--- src/org/onyxplatform/api/java/API.java
+++ src/org/onyxplatform/api/java/API.java
@@ -4,69 +4,66 @@ import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 import clojure.lang.PersistentArrayMap;
 
-public class API {
+public class API implements OnyxNames {
 
-    static {       
-        IFn require = Clojure.var("clojure.core", "require");
-        require.invoke(Clojure.read("onyx.api"));
+    static {
+        IFn require = Clojure.var(CORE, Require);
+        require.invoke(Clojure.read(API));
     }
-    
+
     public static Object startEnv(EnvConfiguration envConfig) {
-        IFn startEnv = Clojure.var("onyx.api", "start-env");        
+        IFn startEnv = Clojure.var(API, StartEnv);
         Object coercedConfig = envConfig.toCljMap();
         return startEnv.invoke(coercedConfig);
     }
-    
+
     public static Object startPeerGroup(PeerConfiguration peerConfig) {
-        IFn startPeerGroup = Clojure.var("onyx.api", "start-peer-group");
+        IFn startPeerGroup = Clojure.var(API, StartPeerGroup);
         Object coercedConfig = peerConfig.toCljMap();
         return startPeerGroup.invoke(coercedConfig);
     }
-    
+
     public static Object startPeers(int nPeers, Object peerGroup) {
-        IFn startPeers = Clojure.var("onyx.api" , "start-peers");
+        IFn startPeers = Clojure.var(API , StartPeers);
         return startPeers.invoke(nPeers, peerGroup);
     }
-    
+
     public static Object shutdownEnv(Object env) {
-        IFn shutdownEnv = Clojure.var("onyx.api", "shutdown-env");
+        IFn shutdownEnv = Clojure.var(API, ShutdownEnv);
         return shutdownEnv.invoke(env);
     }
-    
+
     public static Object shutdownPeerGroup(Object peerGroup) {
-        IFn shutdownPeerGroup = Clojure.var("onyx.api", "shutdown-peer-group");
+        IFn shutdownPeerGroup = Clojure.var(API, ShutdownPeerGroup);
         return shutdownPeerGroup.invoke(peerGroup);
     }
-    
+
     public static Object shutdownPeers(Object peers) {
-        IFn shutdownPeer = Clojure.var("onyx.api", "shutdown-peers");
+        IFn shutdownPeer = Clojure.var(API, ShutdownPeers);
         return shutdownPeer.invoke(peers);
     }
-    
-    public static boolean submitJob(PeerConfiguration peerConfig, Job job) {
 
-        IFn submitJob = Clojure.var("onyx.api", "submit-job");
-        
-	Object coercedJob = job.toCljMap();
+    public static boolean submitJob(PeerConfiguration peerConfig, Job job) {
+        IFn submitJob = Clojure.var(API, SubmitJob);
+	    Object coercedJob = job.toCljMap();
         Object coercedConfig = peerConfig.toCljMap();
-        
         return (boolean) submitJob.invoke(coercedConfig, coercedJob);
     }
-    
+
     public static boolean killJob(PeerConfiguration peerConfig, String jobID) {
-        IFn killJob = Clojure.var("onyx.api", "kill-job");
+        IFn killJob = Clojure.var(API, KillJob);
         Object coercedConfig = peerConfig.toCljMap();
         return (boolean) killJob.invoke(coercedConfig, jobID);
     }
-    
-    public static boolean gc(PeerConfiguration peerConfig) {
-        IFn gc = Clojure.var("onyx.api", "gc");
+
+    public static boolean collectGarbage(PeerConfiguration peerConfig) {
+        IFn collectGarbage = Clojure.var(API, CollectGarbage);
         Object coercedConfig = peerConfig.toCljMap();
-        return (boolean) gc.invoke(coercedConfig);
+        return (boolean) collectGarbage.invoke(coercedConfig);
     }
-    
+
     public static boolean awaitJobCompletion(PeerConfiguration peerConfig, String jobID) {
-        IFn awaitJobCompletion = Clojure.var("onyx.api", "await-job-completion");
+        IFn awaitJobCompletion = Clojure.var(API, AwaitJobCompletion);
         Object coercedConfig = peerConfig.toCljMap();
         return (boolean) awaitJobCompletion.invoke(coercedConfig, jobID);
     }
