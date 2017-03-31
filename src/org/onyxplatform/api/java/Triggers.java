a3f7a3f119032d35fdfafa329ea01a417dea9a12
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 17:36:07 -0400

    added Trigger comments

--- src/org/onyxplatform/api/java/Triggers.java
+++ src/org/onyxplatform/api/java/Triggers.java
@@ -1,16 +1,34 @@
 package org.onyxplatform.api.java;
 
-public class Triggers 
-	extends OnyxVector
+/**
+ * Triggers are a set of Trigger objects that can be collectively applied
+ * to a window or windows associated with an onyx job.
+ * the Triggers object derives from OnyxVector.
+ */
+public class Triggers extends OnyxVector
 {
+	/**
+	 * Creates a new Triggers object using OnyxVector superconstructor.
+	 * @return new Triggers object
+	 */
     public Triggers() {
-	    super();
     }
-    
+
+	/**
+	 * Creates a new Triggers object using an existing Triggers
+	 * object (uses the existing trigger collection as content).
+	 * Uses OnyxVector superconstructor.
+	 * @param  Triggers ts            existing Triggers object to use for content
+	 * @return          new Triggers object
+	 */
     private Triggers(Triggers ts) {
-	    super (ts.vContents);
+	    super(ts.vContents);
     }
-    
+
+	/**
+	 * Adds an existing Trigger to the Triggers content vector.
+	 * @param Trigger te Trigger to add to the Triggers content
+	 */
     public void addTrigger(Trigger te) {
 	    addElement( te );
     }
