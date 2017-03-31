ace34016d65c25f4b5bd545e2c09534eeaa26edb
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 16:18:50 -0400

    added javadoc comments to EnvConfiguration class

--- src/org/onyxplatform/api/java/EnvConfiguration.java
+++ src/org/onyxplatform/api/java/EnvConfiguration.java
@@ -3,20 +3,35 @@ package org.onyxplatform.api.java;
 import clojure.lang.PersistentHashMap;
 import java.util.Map;
 
-public class EnvConfiguration 
-	extends OnyxEntity
+public class EnvConfiguration extends OnyxEntity
 {
 	protected static String coerceKw = "env-config";
 
+		/**
+		 * Calls empty superconstructor for OnyxEntity
+		 * @return newly created EnvConfiguration object
+		 */
     	public EnvConfiguration () {
-	    	super();
     	}
-    
+
+		/**
+		 * Calls OnyxEntity superconstructor with existing environment
+		 * configuration.
+		 * @param  EnvConfiguration cfg           existing environment
+		 *                          		configuration to use for construction
+		 * @return                  newly created EnvConfiguration object
+		 */
     	private EnvConfiguration(EnvConfiguration cfg) {
-	    	super( cfg.entry );
+	    	super(cfg.entry);
     	}
-	
+
+		/**
+		 * Coerces the envConfig content map 'entry' into
+		 * a proper onyx environment configuration.
+		 * @param  Map<String, Object>       jMap content map to coerce
+		 * @return             object representing onyx version of envConfig
+		 */
     	protected PersistentHashMap coerce(Map<String, Object> jMap) {
-		return (PersistentHashMap) super.castTypesFn.invoke( coerceKw, jMap);
+			return (PersistentHashMap) castTypesFn.invoke(coerceKw, jMap);
     	}
 }
