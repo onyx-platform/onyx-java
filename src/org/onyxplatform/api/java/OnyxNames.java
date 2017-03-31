c4bb05dc2b6e5ae93e084871fe8cfb52f2eaeab3
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 13:44:31 -0400

    Organized/labeled OnyxNames interface

--- src/org/onyxplatform/api/java/OnyxNames.java
+++ src/org/onyxplatform/api/java/OnyxNames.java
@@ -1,35 +1,54 @@
 package org.onyxplatform.api.java;
 
 /**
- * This interface represents the base of the onyx-java package.
- * It's purpose is simply to abstract the use of and centralize the location of
- * onyx keywords. This interface is implemented by any base classes which
- * specify interop of Java and Clojure types or behaviors.
+ * Abstracts use of and centralizes location of onyx keywords.
+ * This interface is implemented by any base classes which
+ * uses onyx-api via IFn interop.
  */
 public interface OnyxNames {
 
 
+	/**
+	 * Interop
+	 */
 	public static final String CORE 		= "clojure.core";
+	public static final String INTEROP 		= "onyx.interop";
+	public static final String API 			= "onyx.api";
+
 	public static final String Require 		= "require";
 	public static final String Keyword 		= "keyword";
 
-
-	public static final String API 			= "onyx.api";
-
-	public static final String StartEnv 		= "start-env";
-	public static final String StartPeerGroup 	= "start-peer-group";
-	public static final String StartPeers 		= "start-peers";
-	public static final String ShutdownEnv 		= "shutdown-env";
-	public static final String ShutdownPeerGroup 	= "shutdown-peer-group";
-	public static final String ShutdownPeers 	= "shutdown-peers";
-	public static final String SubmitJob 		= "submit-job";
-
-
-
-	public static final String INTEROP 		= "onyx.interop";
-	public static final String CastTypes 		= "cast-types";
+	/**
+	 * Environment and Peer Configuration
+	 */
 	public static final String EnvConfig 		= "env-config";
 	public static final String PeerConfig 		= "peer-config";
+
+	/**
+	 * Environment and Peer Control
+	 */
+	public static final String StartEnv 		= "start-env";
+	public static final String ShutdownEnv 		= "shutdown-env";
+
+	public static final String StartPeers 		= "start-peers";
+	public static final String ShutdownPeers 	= "shutdown-peers";
+
+	public static final String StartPeerGroup 	= "start-peer-group";
+	public static final String ShutdownPeerGroup 	= "shutdown-peer-group";
+
+	/*
+	Job Control
+	 */
+	public static final String SubmitJob 		= "submit-job";
+
+	/**
+	 * Generic TypeCasting
+	 */
+	public static final String CastTypes 		= "cast-types";
+
+	/**
+	 * Type Coercion
+	 */
 	public static final String CoerceWorkflow 	= "coerce-workflow";
 	public static final String CoerceCatalog 	= "coerce-catalog";
 	public static final String CoerceLifecycles 	= "coerce-lifecycles";
