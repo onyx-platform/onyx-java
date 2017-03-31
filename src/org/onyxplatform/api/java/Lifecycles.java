f686fba0ec7ba61ecca73242ef2b3a5eab33f11c
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 17:09:03 -0400

    added comments for lifecycles

--- src/org/onyxplatform/api/java/Lifecycles.java
+++ src/org/onyxplatform/api/java/Lifecycles.java
@@ -1,17 +1,35 @@
 package org.onyxplatform.api.java;
 
-public class Lifecycles 
-	extends OnyxVector
+/**
+ * Lifecycles are a set of Lifecycle objects that can be applied to
+ * Tasks running as part of a job.
+ * Lifecycles derive from OnyxVector.
+ */
+public class Lifecycles extends OnyxVector
 {
+    /**
+     * Creates a new Lifecycles object using OnyxVector superconstructor.
+     * @return new Lifecycles object
+     */
     public Lifecycles() {
-	    super();
     }
-    
+
+    /**
+     * Creates a new Lifecycles object using an existing Lifecycles object.
+     * Uses OnyxVector superconstructor.
+     * @param  Lifecycles ls            existing Lifecycles object containing
+     *                              contents to use for new Lifecycles object
+     * @return            new Lifecycles object
+     */
     private Lifecycles(Lifecycles ls) {
-	    super( ls.vContents );
+	    super(ls.vContents);
     }
-    
+
+    /**
+     * Adds an existing Lifecycle to the Lifecycles content vector.
+     * @param Lifecycle cs Lifecycle to add to the Lifecycles contents.
+     */
     public void addCall(Lifecycle cs) {
-	    super.vContents = super.vContents.cons( cs );
+	    vContents = vContents.cons(cs);
     }
 }
