2c7b56d117a6a1b4c69e3a455476b0daec24e934
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 18:15:38 -0400

    formatting

--- src/org/onyxplatform/api/java/OnyxNames.java
+++ src/org/onyxplatform/api/java/OnyxNames.java
@@ -11,52 +11,52 @@ public interface OnyxNames {
 	/**
 	 * Interop
 	 */
-	public static final String CORE 		= "clojure.core";
-	public static final String INTEROP 		= "onyx.interop";
-	public static final String API 			= "onyx.api";
+	public static final String CORE 				= "clojure.core";
+	public static final String INTEROP 				= "onyx.interop";
+	public static final String API 					= "onyx.api";
 
-	public static final String Require 		= "require";
-	public static final String Keyword 		= "keyword";
+	public static final String Require 				= "require";
+	public static final String Keyword 				= "keyword";
 
 	/**
 	 * Environment and Peer Configuration
 	 */
-	public static final String EnvConfig 		= "env-config";
-	public static final String PeerConfig 		= "peer-config";
+	public static final String EnvConfig 			= "env-config";
+	public static final String PeerConfig 			= "peer-config";
 
 	/**
 	 * Environment and Peer Control
 	 */
-	public static final String StartEnv 		= "start-env";
-	public static final String ShutdownEnv 		= "shutdown-env";
+	public static final String StartEnv 			= "start-env";
+	public static final String ShutdownEnv 			= "shutdown-env";
 
-	public static final String StartPeers 		= "start-peers";
-	public static final String ShutdownPeer		= "shutdown-peer";
-	public static final String ShutdownPeers 	= "shutdown-peers";
+	public static final String StartPeers 			= "start-peers";
+	public static final String ShutdownPeer			= "shutdown-peer";
+	public static final String ShutdownPeers 		= "shutdown-peers";
 
-	public static final String StartPeerGroup 	= "start-peer-group";
+	public static final String StartPeerGroup 		= "start-peer-group";
 	public static final String ShutdownPeerGroup 	= "shutdown-peer-group";
 
 	/*
 	Job Control
 	 */
-	public static final String SubmitJob 		= "submit-job";
-	public static final String KillJob 		= "kill-job";
-	public static final String AwaitJobCompletion = "await-job-completion";
-	public static final String CollectGarbage = "gc";
+	public static final String SubmitJob 			= "submit-job";
+	public static final String KillJob 				= "kill-job";
+	public static final String AwaitJobCompletion 	= "await-job-completion";
+	public static final String CollectGarbage 		= "gc";
 
 	/**
 	 * Generic TypeCasting
 	 */
-	public static final String CastTypes 		= "cast-types";
+	public static final String CastTypes 			= "cast-types";
 
 	/**
 	 * Type Coercion
 	 */
-	public static final String CoerceWorkflow 	= "coerce-workflow";
-	public static final String CoerceCatalog 	= "coerce-catalog";
+	public static final String CoerceWorkflow 		= "coerce-workflow";
+	public static final String CoerceCatalog 		= "coerce-catalog";
 	public static final String CoerceLifecycles 	= "coerce-lifecycles";
 	public static final String CoerceFlowConditions = "coerce-flow-conditions";
-	public static final String CoerceWindows 	= "coerce-windows";
-	public static final String CoerceTriggers 	= "coerce-triggers";
+	public static final String CoerceWindows 		= "coerce-windows";
+	public static final String CoerceTriggers 		= "coerce-triggers";
 }
