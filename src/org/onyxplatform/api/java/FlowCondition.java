3a6adc593837ed1dd9873b3834d30aa9745e7739
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-03-31 16:56:30 -0400

    added javadoc comments to FlowCondition

--- src/org/onyxplatform/api/java/FlowCondition.java
+++ src/org/onyxplatform/api/java/FlowCondition.java
@@ -3,20 +3,41 @@ package org.onyxplatform.api.java;
 import clojure.lang.PersistentHashMap;
 import java.util.Map;
 
-public class FlowCondition
-	extends OnyxEntity
+/**
+ * Flow conditions are used for isolating logic about whether or not segments
+ * should pass through different tasks in a workflow, and support a rich degree
+ * of composition with runtime parameterization.
+ * Flow conditions derive from OnyxEntity.
+ */
+public class FlowCondition extends OnyxEntity
 {
 	protected static String coerceKw = "flow-conditions-entry";
-    
-    	public FlowCondition() {
-        	entry = PersistentHashMap.EMPTY;
-    	}
-    	
-    	private FlowCondition(PersistentHashMap ent) {
-        	entry = ent;
-    	}
-	
+
+	/**
+	 * Creates a new FlowCondition object using OnyxEntity superconstructor.
+	 * @return new FlowCondition object.
+	 */
+	public FlowCondition() {
+    	entry = PersistentHashMap.EMPTY;
+	}
+
+	/**
+	 * Creates a new FlowCondition object using an existing content map.
+	 * Uses OnyxEntity superconstructor.
+	 * @param  PersistentHashMap ent           existing map to use for new FlowCondition
+	 * @return                  new FlowCondition object
+	 */
+	private FlowCondition(PersistentHashMap ent) {
+    	entry = ent;
+	}
+
+	/**
+	 * Coerces FlowConditions object content map into proper onyx FlowCondition.
+	 * Returns the onyx representation without altering the existing content map.
+	 * @param  Map<String, Object>       jMap Content map to coerce.
+	 * @return             onyx representation of content map
+	 */
 	protected PersistentHashMap coerce(Map<String, Object> jMap) {
-		return (PersistentHashMap) super.castTypesFn.invoke( coerceKw, jMap);
+		return (PersistentHashMap) castTypesFn.invoke(coerceKw, jMap);
 	}
 }
