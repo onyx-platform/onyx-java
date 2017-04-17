6143badc6538d31e25dd6cd84c82a14b33c1e1b6
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2017-04-17 12:47:16 -0400

    Added constructor concept to object factory (this is so that we can specify using EDN files which constructor to use and what parameters to use for our persistent objects). This behavior is required for dealing with ENUM types (TaskScheduler) and arbitrary argument constructors (Job)

--- src/onyx_java/test.clj
+++ src/onyx_java/test.clj
@@ -1,24 +1,28 @@
 (ns onyx-java.test
     (:gen-class)
     (:require [onyx-java.utils.test-template :as t]
+              [onyx-java.utils.object :as o]
               [clojure.test :refer [deftest is]]))
 
+(def construct-dir "test/resources/onyx_simple/construct")
 (def input-dir "test/resources/onyx_simple/source/")
 (def output-dir "test/resources/onyx_simple/target/")
 
 (deftest entity-coercion-test
-    (let [input input-dir
+    (let [construct construct-dir
+          input input-dir
           output output-dir
-          master-map (t/create-master-map input)
+          master-map (t/create-master-map construct)
           params (t/add-entity-params master-map input)
           coerced-map (t/coerce-entities master-map)
           expected-map (t/get-expected-entities master-map output)]
     (is (= coerced-map expected-map))))
 
 (deftest vector-coercion-test
-    (let [input input-dir
+    (let [construct construct-dir
+          input input-dir
           output output-dir
-          master-map (t/create-master-map input)
+          master-map (t/create-master-map construct)
           params (t/add-entity-params master-map input)
           entities (t/add-vector-entities master-map)
           coerced-map (t/coerce-vectors master-map)
@@ -27,9 +31,10 @@
            (t/vector-frequency expected-map)))))
 
 (deftest workflow-coercion-test
-    (let [input input-dir
+    (let [construct construct-dir
+          input input-dir
           output output-dir
-          master-map (t/create-master-map input)
+          master-map (t/create-master-map construct)
           edges (t/add-workflow-edges master-map input)
           coerced-map (t/coerce-workflows master-map)
           expected-map (t/get-expected-workflows master-map output)]
@@ -37,9 +42,10 @@
                  (t/vector-frequency expected-map)))))
 
 (defn job-coercion-test []
-    (let [input input-dir
+    (let [construct construct-dir
+          input input-dir
           output output-dir
-          master-map (t/create-master-map input)
+          master-map (t/create-master-map construct)
           params (t/add-entity-params master-map input)
           entities (t/add-vector-entities master-map)
           edges (t/add-workflow-edges master-map input)
