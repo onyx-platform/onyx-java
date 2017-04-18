b332f6f4e717c07a22bf7858e91ed7d28f44c7e6
George Kierstein <gateway.spectacle@gmail.com>
2017-04-18 16:03:26 -0400

    put back right main

--- project.clj
+++ project.clj
@@ -12,8 +12,8 @@
   :resource-paths ["test/resources"]
   :repl-options {:init-ns onyx-java.entity-test
                  :caught clj-stacktrace.repl/pst+ }
-  :main ^:skip-aot onyx-java.test
   :prep-tasks  [["compile"] "javac" "compile"]
+  :main ^:skip-aot onyx-java.entity-test
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :plugins [[lein-update-dependency "0.1.2"]
