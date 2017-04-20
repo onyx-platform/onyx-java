83c6c313c531fa5c0252256097b70fd8e1c0411d
George Kierstein <gateway.spectacle@gmail.com>
2017-04-20 15:41:31 -0400

    cleaned up comments

--- src/clj/onyx_java/utils/vector_fns.clj
+++ src/clj/onyx_java/utils/vector_fns.clj
@@ -4,6 +4,5 @@
            org.onyxplatform.api.java.OnyxMap))
 
 (defn keywordize-str-array [string-array]
-  ; TODO fully realize the lazy seq into 
   (into [] (map #(keyword %) (vec string-array))))
 
