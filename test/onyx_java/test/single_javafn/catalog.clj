4429e5b00ade83ea5875c4362c4c8053d5825da3
George Kierstein <gateway.spectacle@gmail.com>
2017-04-21 14:54:05 -0400

    fixed bug in Catalog loading

--- test/onyx_java/test/single_javafn/catalog.clj
+++ test/onyx_java/test/single_javafn/catalog.clj
@@ -6,14 +6,38 @@
 
 (defn build-catalog []
   (let [cat (Catalog.)
-        om ()
+        om (MapFns/fromResources "catalog-simple.edn")
         t (Task. om) ]
     (-> cat
         (.addTask t)
-        
-        )))
+        (AsyncCatalog/addInput  "in" 5 50)
+        (AsyncCatalog/addOutput "out" 5 50))))
+
+(def expected [{:onyx/name :jfn, 
+                :onyx/fn :onyx_java.test.Functions/blurtHI, 
+                :onyx/type :function, 
+                :onyx/batch-size 5   
+                :onyx/batch-timeout 50}
+
+               {:onyx/name :in, 
+                :onyx/plugin :onyx.plugin.core-async/input, 
+                :onyx/medium :core.async, 
+                :onyx/type :input, 
+                :onyx/max-peers 1, 
+                :onyx/batch-size 5   
+                :onyx/batch-timeout 50, 
+                :onyx/doc "Reads segments from a core.async channel"}
+               
+               {:onyx/name :out, 
+                :onyx/plugin :onyx.plugin.core-async/output, 
+                :onyx/medium :core.async, 
+                :onyx/type :output, 
+                :onyx/max-peers 1, 
+                :onyx/batch-size 5  
+                :onyx/batch-timeout 50, 
+                :onyx/doc "Writes segments to a core.async channel"}])
 
 (deftest valid-catalog?
   (let [cat (build-catalog)]
-    (is true)))
+    (is (= expected (.tasks cat)))))
 
