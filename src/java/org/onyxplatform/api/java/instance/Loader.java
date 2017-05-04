5e873f1d52c704aa61e095eee1d4063af76ec4eb
George Kierstein <gateway.spectacle@gmail.com>
2017-05-04 15:04:48 -0400

    expanding support for custom classloading to allow for memory management

--- src/java/org/onyxplatform/api/java/instance/Loader.java
+++ src/java/org/onyxplatform/api/java/instance/Loader.java
@@ -7,14 +7,33 @@ import java.lang.reflect.Constructor;
 
 public class Loader {
 
-	// TODO: Create a custom class loader so that 
-	//       the class will be garbage collected 
-	//       when its no longer referenced. 
+	// Custom Class Loader ----------------------------------------
 	//
-	//       Otherwise the class will remain in 
-	//       memory and any native library 
-	//       loaded into it will not be unloaded.
+	// We use a custom class loader so that 
+	// the class will be garbage collected 
+	// when its no longer referenced. 
 	//
+	// Otherwise the class will remain in 
+	// memory and any native library 
+	// loaded into it will never be released.
+	//
+
+	// TODO: Add releaseClasses method to custom class loader
+	//       which nukes the Class cache.
+	//
+
+	public static Class<?> findClass(ClassLoader cl, String fqClassName) 
+		throws ClassNotFoundException
+	{
+		if (cl == null) {
+			return Class.forName(fqClassName);
+		}
+		else {
+
+			return null;
+		}
+	}
+
 	public static IFn loadOnyxFn(String fqClassName, IPersistentMap args) 
 		throws ClassNotFoundException, 
 		NoSuchMethodException, 
@@ -22,11 +41,19 @@ public class Loader {
 		IllegalAccessException,
 		java.lang.reflect.InvocationTargetException
 	{
-		Class<?> ifnClazz = Class.forName(fqClassName);
-	        Class<?> ipmClazz = Class.forName("clojure.lang.IPersistentMap");	
+		// Use the environment-wide default class loader for 
+		// general classes we want permanently cached.
+	        Class<?> ipmClazz = findClass(null, "clojure.lang.IPersistentMap");
+
+		// Use a custom class loader for instances we want to 
+		// unload during gc.
+		ClassLoader loader = null;
+		Class<?> ifnClazz = findClass(loader, fqClassName);
+
 		Constructor ctr = ifnClazz.getConstructor(new Class[] { ipmClazz });
-		IFn instance = (IFn)ctr.newInstance(new Object[] { args });
-		return instance;
+		OnyxFn instance = (OnyxFn)ctr.newInstance(new Object[] { args });
+		instance.setClassLoader(loader);
+		return (IFn)instance;
 	}
 }
 
--- src/java/org/onyxplatform/api/java/instance/OnyxFn.java
+++ src/java/org/onyxplatform/api/java/instance/OnyxFn.java
@@ -12,20 +12,43 @@ import org.onyxplatform.api.java.utils.MapFns;
 
 public abstract class OnyxFn extends AFn implements OnyxNames {
 
-	protected IPersistentMap cntrArgs;
+	// Class -------------------------------------------
+	//
+	// The ClassLoader member is added by the Loader
+	// with affordance for null-ing it out when this object
+	// is removed. This ensures that when all instances 
+	// of the class are no longer referenced the class 
+	// itself will also be garbage collected. 
+	//
+	// This matters for native code-backed class loading.
+	// Java has no affordances for unloading libraries
+	// pulled in by classes because the library is class-level bound
+	// and the class itself is never gc'ed if loaded by 
+	// the default class loader which is always reachable.
+	//
 
-	// TODO: Add a ClassLoader member that is added by the Loader
-	//       with affordance for null-ing it out when this object
-	//       is removed. This ensures that when all instances 
-	//       of the class are no longer referenced the class 
-	//       itself will also be garbage collected. 
-	//
-	//       This matters for native code-backed class loading.
-	//       Java has no affordances for unloading libraries
-	//       pulled in by classes because the library is class-level bound
-	//       and the class itself is never gc'ed if loaded by 
-	//       the default class loader which is always reachable.
-	//
+	protected static ClassLoader classLoader = null;
+
+	public static void setClassLoader(ClassLoader cl) {
+		classLoader = cl;
+	}
+
+	public void releaseClassLoader() {
+		// TODO: Release all class references in the class loader
+		classLoader = null;
+	}
+
+	public static Class<?> findClass(String fqClassName) 
+		throws ClassNotFoundException
+	{
+		return Loader.findClass(classLoader, fqClassName);
+	}
+
+
+	// Instance --------------------------------------
+	// 
+
+	protected IPersistentMap cntrArgs;
 
 	/**
 	 * This MUST be overridden by the concrete subclass. 
